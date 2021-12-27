3aacb00ae84204d3d2e1a55eae4932caf4610897
filippob <filippo.biscarini@gmail.com>
2021-12-27 09:54:09 +0100

    adding comments on script 2.

--- scripts/2.descriptive_stats.sh
+++ scripts/2.descriptive_stats.sh
@@ -11,7 +11,10 @@ if [ ! -d "$OUTDIR" ]; then
 	mkdir $OUTDIR
 fi
 
-## plink commands
+## plink commands 
+## (option --cow to allow for 60 chromosomes in goats)
+## there is no option --goat 
+## (sheep has 54 chromosomes, therefore --sheep would not work)
 $PLINK --cow --allow-extra-chr --bfile $DATA --missing --family --out $OUTDIR/goats
 $PLINK --cow --allow-extra-chr --bfile $DATA --freq --family --out $OUTDIR/goats
 
