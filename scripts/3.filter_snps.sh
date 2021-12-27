1eca7c84c5de5df3ff76dfd9b3cc859e7c02678c
filippob <filippo.biscarini@gmail.com>
2021-12-27 15:45:59 +0100

    adding option --make-bed to the filtering script

--- scripts/3.filter_snps.sh
+++ scripts/3.filter_snps.sh
@@ -18,7 +18,7 @@ fi
 ## (option --cow to allow for 60 chromosomes in goats)
 ## there is no option --goat 
 ## (sheep has 54 chromosomes, therefore --sheep would not work)
-$PLINK --cow --allow-extra-chr --bfile $DATA --chr 1-29 --bp-space 1 --geno 0.05 --mind 0.10 --maf 0.05 --recode --out $OUTDIR/goat_filtered
+$PLINK --cow --allow-extra-chr --bfile $DATA --chr 1-29 --bp-space 1 --geno 0.05 --mind 0.10 --maf 0.05 --make-bed --out $OUTDIR/goat_filtered
 
 echo "DONE!"
 
