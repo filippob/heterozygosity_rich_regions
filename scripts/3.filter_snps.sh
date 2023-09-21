33b5cbf2ee005cea3877f15ad26ebb7d8b46e7cc
filippob <filippo.biscarini@gmail.com>
2023-09-21 15:55:33 +0200

    script modified to work with vcf file and to update the map (maybe this last task can go to a separate script later)

--- scripts/3.filter_snps.sh
+++ scripts/3.filter_snps.sh
@@ -4,12 +4,13 @@
 ## (detection of common HRR across commercial and rural goat populations)
 
 # DATA=/home/smarter/hrr_goat_data/goats
-DATA=/home/smarter/hrr_parameters/data/select_sheep_lacaune_SMARTEROAR-top-0.4.7dev_only50
+DATA=/home/smarter/human_hrr/Analysis/CEU/CEU.vcf.gz
 # PLINK=$HOME/software/plink/plink
 PLINK=$HOME/software/plink/plink
 # OUTDIR=$HOME/hrr/Analysis
-OUTDIR=/home/smarter/hrr_parameters/Analysis/filter
-SPECIES="sheep"
+OUTDIR=/home/smarter/human_hrr/Analysis/CEU/filter
+SPECIES="CEU"
+newmap=/home/smarter/human_hrr/Analysis/CEU/new_map
 MAF=0.05
 GENO=0.05
 MIND=0.10
@@ -22,7 +23,16 @@ fi
 ## (option --cow to allow for 60 chromosomes in goats)
 ## there is no option --goat 
 ## (sheep has 54 chromosomes, therefore --sheep would not work)
-$PLINK --sheep --allow-extra-chr --bfile $DATA --chr 1-26 --bp-space 1 --geno $GENO --mind $MIND --maf $MAF --make-bed --out $OUTDIR/${SPECIES}_filtered
+
+echo " - filtering out SNPs with excess missing rate (${GENO} and low frequency ($MAF))"
+$PLINK --allow-extra-chr --vcf $DATA --autosome --bp-space 1 --geno $GENO --maf $MAF --make-bed --out $OUTDIR/temp
+
+echo " - filtering out samples with excess missing genotypes (threshold: ${MIND})"
+$PLINK --allow-extra-chr --bfile $OUTDIR/temp --autosome --mind $MIND --maf $MAF --update-map $newmap --make-bed --out $OUTDIR/${SPECIES}_autosomes_filtered
+
+## removing temporary file
+echo " - removing temporary files"
+rm $OUTDIR/temp*
 
 echo "DONE!"
 
